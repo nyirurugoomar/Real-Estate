f285e1ca903003db80fd64e8ba331492c1729b45
nyirurugoomar <nyirurugoomar@gmail.com>
2024-05-21 16:30:22 +0200

    fixing slide styles

--- src/components/SlidePage.jsx
+++ src/components/SlidePage.jsx
@@ -1,11 +1,20 @@
-// src/components/SlidePage.js
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
 
 const images = [
-  'https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D',
-  'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlfGVufDB8fDB8fHww',
+  "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlfGVufDB8fDB8fHww",
+];
+
+const texts = [
+  "House 1",
+  "House 2",
+  "House 3",
+];
+const HouseLink = [
+  "Link 1",
+  "Link 2",
+  "Link 3",
 ];
 
 function SlidePage() {
@@ -19,23 +28,38 @@ function SlidePage() {
   }, []);
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden mt-10">
       {images.map((image, index) => (
         <div
           key={index}
-          className={`absolute w-full h-full transition-opacity duration-1000 ease-in-out ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
+          className={`absolute w-full h-full transition-opacity duration-1000 ease-in-out ${
+            currentIndex === index ? "opacity-100" : "opacity-0"
+          }`}
         >
-          <img src={image} alt={`Slide ${index + 1}`} className="w-full h-full object-cover" />
+          <img
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className="w-full h-full object-cover md:p-20"
+          />
         </div>
       ))}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`w-3 h-3 rounded-full ${currentIndex === index ? 'bg-white' : 'bg-gray-400'}`}
-            onClick={() => setCurrentIndex(index)}
-          />
-        ))}
+
+      <div className="absolute bottom-[1px] left-1/2 transform -translate-x-1/2 flex flex-col items-center space-y-2">
+        <div className="text-center mt-4">
+          <h1 className="text-black font-bold text-[24px]">{texts[currentIndex]}</h1>
+          <p className="text-[#69ABE0]">{HouseLink[currentIndex]}</p>
+        </div>
+        <div className="flex space-x-2">
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`w-3 h-3 rounded-full cursor-pointer ${
+                currentIndex === index ? "bg-[#0F75BC]" : "bg-gray-400"
+              }`}
+              onClick={() => setCurrentIndex(index)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
