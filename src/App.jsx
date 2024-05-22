ce189d98fc3b313f9f9106358fb7243d1b7bb687
nyirurugoomar <nyirurugoomar@gmail.com>
2024-05-22 15:24:55 +0200

    Creating Routing path

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import './App.css'
-import About from './components/About'
-import Banner from './components/Banner'
-import Contact from './components/Contact'
-import FAQ from './components/FAQ'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Hero from './components/Hero'
-import SlidePage from './components/SlidePage'
-import Strategies from './components/Strategies'
-
+import { BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
 function App() {
   
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Strategies/>
-      <About/>
-      <SlidePage/>
-      <Banner/>
-      <FAQ/>
-      <Contact/>
-      <Footer/>
+      <div className=''>
+        <Router>
+          <Header/>
+              <Switch>
+              <Route exact path='/' component={Home}/>
+              </Switch>
+            <Footer/>
+        </Router>
+        
+      </div>
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from './Hero'
+import Strategies from './Strategies'
+import About from './About'
+import SlidePage from './SlidePage'
+import Banner from './Banner'
+import FAQ from './FAQ'
+import Contact from './Contact'
+
+function Home() {
+  return (
+    <>
+      <Hero/>
+      <Strategies/>
+      <About/>
+      <SlidePage/>
+      <Banner/>
+      <FAQ/>
+      <Contact/>
+      
+    </>
+  )
+}
+
+export default Home
