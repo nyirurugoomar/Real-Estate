aafb38b4e5c5d47313fc4571c7b586fe2e957d5a
nyirurugoomar <nyirurugoomar@gmail.com>
2024-05-25 12:55:49 +0200

    customize about and services page

--- src/components/page/Services.jsx
+++ src/components/page/Services.jsx
@@ -10,11 +10,11 @@ function Services() {
         </div>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-2 gap-y-16 px-4'>
            <div className='p-16 w-[641px]'>
-              <h2 className='text-black font-rajdhani text-[48px] md:ml-8'>Joint Ventures</h2>
+              <h2 className='text-black font-rajdhani text-[48px] '>Joint Ventures</h2>
               <p className='text-start'>1Up assumes the role of managing partner, providing shared resources, knowledge, expertise, credibility, and capital, allowing investors to achieve ambitious goals and realize their dreams.</p>
               <div className='mt-4'>
                 <h3 className='font-bold font-rajdhani text-[20px]'>Benefits of entering a Joint Venture with 1Up:</h3>
-                <div className='space-y-6 ml-6'>
+                <div className='space-y-6'>
                     <li className='inline text-start'> <span className='text-3xl'>.</span> {" "} Access lucrative real estate opportunities and maximize your investment potential by pooling resources with other investors.</li>
                     <br/>
                     <li className='inline'> <span className='text-3xl'>.</span> {" "}Enjoy shared risk and reward with other investors in the joint venture while benefiting from a hands-off investment approach.</li>
@@ -56,7 +56,7 @@ function Services() {
               <p className='text-start'>Our private lending services provide a stable return on your capital through secured real estate loans with low risk and flexible loan terms. We perform thorough underwriting and due diligence for high-quality lending opportunities.</p>
               <div className='mt-4'>
                 <h3 className='font-bold font-rajdhani text-[20px]'>Benefits of Private Lending with 1Up:</h3>
-                <div className='space-y-6 ml-6'>
+                <div className='space-y-6 '>
                     <li className='inline text-start'> <span className='text-3xl'>.</span> {" "} Access to secured real estate loans with low risk and short-term maturity.</li>
                     <br/>
                     <li className='inline'> <span className='text-3xl'>.</span> {" "}Utilize registered funds, lines of credit, and other financing options to capitalize investment with the aim of earning a return.</li>
