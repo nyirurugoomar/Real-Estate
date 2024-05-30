1d7d32667a716ec175c51f4bf65c11bed083d22b
nyirurugoomar <nyirurugoomar@gmail.com>
2024-05-30 13:34:52 +0200

    adding Booking calenda

---
+++ src/components/Calendal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InlineWidget } from "react-calendly";
+
+const Calendal = () => {
+  return (
+    <div className="App">
+      <InlineWidget url="https://calendly.com/nyirurugoomar" />
+    </div>
+  );
+};
+
+export default Calendal;
